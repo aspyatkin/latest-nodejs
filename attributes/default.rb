2468319668535fd6f64a34437a17013989f053b1
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2016-02-10 19:43:31 +0400

    Update nodejs versions

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,11 +4,11 @@ default[id][:install] = 'lts'
 default[id][:binary] = false
 
 default[id][:lts][:version] = '4.2.6'
-default[id][:lts][:source][:checksum] = 'ea5e357db8817052b17496d607c719d809ed1383e8fcf7c8ffc5214e705aefdd'
-default[id][:lts][:binary][:checksum][:linux_x86] = 'a96ce235e67e994aa5adec8255e13d4fbccd9c5b09b5e9554347dac2b1c5c8a1'
-default[id][:lts][:binary][:checksum][:linux_x64] = '656d8bff06cc5e108b83176f81de7e1eb16392ae0958ec4a7bca2a3a309333a1'
+default[id][:lts][:source][:checksum] = '18504ac6d903cd061f60a29dafcda416a078112f3404d23a7901c41a8e9706b9'
+default[id][:lts][:binary][:checksum][:linux_x86] = '6972ed77c36f026498a0fde6b237fbc554325fa3a7426ee17ce563bdc08caa69'
+default[id][:lts][:binary][:checksum][:linux_x64] = '90ce6e23ad9748813742e1cf09e86fa4c0f3d53972d5dbe920a38bcc842e2d09'
 
-default[id][:stable][:version] = '5.5.0'
-default[id][:stable][:source][:checksum] = 'd69b18cc20699a35434858fb853997616762280610a510ec4b4ff1a94798b432'
-default[id][:stable][:binary][:checksum][:linux_x86] = 'e384f8beb392cd5df882cd3401e449371190bc139010d391888c2f3346fb5107'
-default[id][:stable][:binary][:checksum][:linux_x64] = '3e593d91b6d2ad871efaaf8e9a17b3608ca98904959bcfb7c42e6acce89e80f4'
+default[id][:stable][:version] = '5.6.0'
+default[id][:stable][:source][:checksum] = '3af2cc5e5970afc83e59f2065fea2e2df846a544a100cd3c0527f0db05bec27f'
+default[id][:stable][:binary][:checksum][:linux_x86] = 'f6fc3391f48a3fc2d077dc0e1673fc3934eb2b9465cbeab334e3967d1503ba49'
+default[id][:stable][:binary][:checksum][:linux_x64] = '6b10e446b5a1227673b87d840e9a500f5d2dbd2b806d96e2d81d634c3381a5f1'
