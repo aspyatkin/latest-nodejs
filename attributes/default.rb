6d8c18c1d44f71f74da0694f736354d1a2dd6e61
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2016-03-18 16:25:54 +0400

    Update nodejs stable version to 5.9.0

--- attributes/default.rb
+++ attributes/default.rb
@@ -8,7 +8,7 @@ default[id][:lts][:source][:checksum] = '2cfc76292576d17a8f2434329221675972c96e5
 default[id][:lts][:binary][:checksum][:linux_x86] = '53eb070a459838fb5bff9edc8260b6d0007409d36b8f2917bc1e523e92d6d99f'
 default[id][:lts][:binary][:checksum][:linux_x64] = '114a865effcff2783022ef0fcd30d1e51624d6c28140db0bdc662bcd0f850d8b'
 
-default[id][:stable][:version] = '5.8.0'
-default[id][:stable][:source][:checksum] = 'e8cd580cec1c62053e6357633b9e46d49df1fd5b9fc5c804ca59ba80d722b898'
-default[id][:stable][:binary][:checksum][:linux_x86] = 'b302ce9d5d156857576d892cfe8108363b30c20bd3133011fb2050d56bb238e8'
-default[id][:stable][:binary][:checksum][:linux_x64] = '3d33efa2421ed0769cc23ac172203b7db9fe4914a186009ddbac28c9aadb0896'
+default[id][:stable][:version] = '5.9.0'
+default[id][:stable][:source][:checksum] = 'e5175a66481fd9389ef74ef8e8c164d052f139664deffc9e79241d6234760f3e'
+default[id][:stable][:binary][:checksum][:linux_x86] = 'fca2df390ff964951cc2d18150d17dfe474e5b59864415976b8bad9452f725eb'
+default[id][:stable][:binary][:checksum][:linux_x64] = '99c4136cf61761fac5ac57f80544140a3793b63e00a65d4a0e528c9db328bf40'
--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name 'latest-nodejs'
 maintainer 'Alexander Pyatkin'
 maintainer_email 'aspyatkin@gmail.com'
 license 'MIT'
-version '1.2.3'
+version '1.2.4'
 description 'Installs and configures nodejs'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 
