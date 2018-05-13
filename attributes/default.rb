6b36690d89abf2d43804c30bfcb0b36aae5128a9
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2018-05-13 17:15:32 +0400

    Update versions

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,20 +1,20 @@
 id = 'latest-nodejs'
 
 default[id]['install'] = 'lts'
-default[id]['binary'] = false
+default[id]['binary'] = true
 
-default[id]['lts']['version'] = '8.9.1'
+default[id]['lts']['version'] = '8.11.1'
 default[id]['lts']['source']['checksum'] = \
-  '32491b7fcc4696b2cdead45c47e52ad16bbed8f78885d32e873952fee0f971e1'
+  '86678028f13b26ceed08efc4b838921ca1bf514c0b7e8151bfec8ba15c5e66ad'
 default[id]['lts']['binary']['checksum']['linux_x86'] = \
-  '830f5f4ff29c2b30089a19e1b71d52d02e965b4e1f08282a09616d99aae1a42b'
+  '373e28cb9549fda7063b35eba84827fb0fda6c01856d0b310431eaea7d0c6d45'
 default[id]['lts']['binary']['checksum']['linux_x64'] = \
-  '0e49da19cdf4c89b52656e858346775af21f1953c308efbc803b665d6069c15c'
+  '0e20787e2eda4cc31336d8327556ebc7417e8ee0a6ba0de96a09b0ec2b841f60'
 
-default[id]['current']['version'] = '9.1.0'
+default[id]['current']['version'] = '10.1.0'
 default[id]['current']['source']['checksum'] = \
-  '42dc9f4fe1db689284d5e848963a4f9af1f5092a9a167a890ccd88dcd6fd51df'
+  'e4b5a1d9fac4b20b5421afe34108867df359c69c4c4e2ad6e2bd75b113ddf0db'
 default[id]['current']['binary']['checksum']['linux_x86'] = \
-  'f2b0783f8be2d104e436e05b5f8477f4250f8af7cf785320ba6721546017d2d9'
+  'n/a'
 default[id]['current']['binary']['checksum']['linux_x64'] = \
-  '5a05b32e677c6d5c2e85df30663aa1898aa8fe45ac6b797554069c97b9a5d228'
+  'a750eaa8dd2abf175216377da86ed24d1265597b86c542fcf9cdf7b8043e006e'
--- metadata.rb
+++ metadata.rb
@@ -2,13 +2,13 @@ name 'latest-nodejs'
 maintainer 'Alexander Pyatkin'
 maintainer_email 'aspyatkin@gmail.com'
 license 'MIT'
-version '1.5.0'
+version '1.6.0'
 description 'Installs and configures Node.js'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 
 recipe 'latest-nodejs::default', 'Installs and configures Node.js from source'
 recipe 'latest-nodejs::from_binary', 'Installs and configures Node.js from binary'
-depends 'nodejs', '~> 4.0.0'
+depends 'nodejs', '~> 5.0.0'
 
 source_url 'https://github.com/aspyatkin/latest-nodejs-cookbook' if respond_to?(:source_url)
 
